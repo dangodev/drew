e5bf76891bce54f010fe2b1a92537c3751d79e21
Drew Powers <drew@pow.rs>
2020-03-08 21:47:49 -0600

    Add blog link to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,6 +4,8 @@ import style from './style.css';
 const Header = () => (
   <header className={style.header}>
     <h1>Drew Powers</h1>
+    <a href="https://blog.pow.rs">Blog</a>
+    <a href="https://blog.pow.rs/about">About</a>
   </header>
 );
 
